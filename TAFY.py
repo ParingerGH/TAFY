173517a48e64461bf3589eca9d01406d26934587
ParingerGH <61421117+ParingerGH@users.noreply.github.com>
2021-03-15 21:35:15 +0300

    + Левосторонний

--- TAFY.py
+++ TAFY.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[11]:
 
 
 rules = dict()
@@ -21,21 +21,24 @@ ready = []
 stack.append(string)
 no_not_term = 0 
 
-
-
 # первое попавшееся правило обход
+# если беск то strings цепочек
 
 while len(stack) > 0 and len(ready) <= strings:
     
     #отладка
     print("------------------------------")
     print("Stack: ",stack)                  
-    print("Ready: ",ready)
+    print("Ready: ",ready,"\n")
     
     for key in rules:
         
         # проверка, что в текущей строке остались только терминалы
         if no_not_term == len(rules):
+            
+            #отладка
+            print(stack[len(stack)-1]," -> ready")
+                
             ready.append(stack.pop(len(stack)-1))
             no_not_term = 0
             continue
@@ -58,6 +61,89 @@ while len(stack) > 0 and len(ready) <= strings:
             else:
                 no_not_term += 1
                 continue
+            
+print("\n Цепочки: \n")
+for i in range(len(ready)):
+    print(i,") ",ready[i])
+
+
+# In[81]:
+
+
+def iterate_left(stack,ready,rules,debug):
+    
+    no_not_term = 0 
+    
+    #ОТЛАДКА
+    if debug:
+        print("------------------------------")
+        print("Stack: ",stack)                  
+        print("Ready: ",ready,"\n")
+    #--------------------------------------
+    
+    # проходим все элементы строки СЛЕВА на право (1)
+    for char in stack[len(stack)-1]:
+        
+        #ОТЛАДКА
+        if debug:
+            print("Нетерминальные сиволы в сстроке ",no_not_term / len(rules))
+        #-----------------------------------------------------------------
+        
+        # каждый элемент сравниваем с ключом из правил
+        for key in rules:
+            
+            #ОТЛАДКА
+            if debug:
+                print("Check ",char," for ",key," in ",stack[len(stack)-1])
+            #-----------------------------------------------------------------
+                   
+            # если элемент это ключ из правил, то заменяем (2)
+            if char == key:
+                no_not_term = 0
+                tmp = stack.pop(len(stack)-1)
+
+                for j in range(len(rules[key])):
+                        stack.append(tmp.replace(key, rules[key][j], 1))
+                        
+                        #ОТЛАДКА
+                        if debug:
+                            print("Замена нетерминала",key," на ",rules[key][j])
+                        #---------------------------------------------------
+                
+                        
+                        
+                return
+            # если элемент не является хоть одним из ключей (3)
+            else:
+                no_not_term += 1
+                
+            # проверка, что в текущей строке остались только терминалы (3)
+            # если каждый элемент это не ключ     
+            if no_not_term == len(rules)*len(stack[len(stack)-1]):
+
+                #ОТЛАДКА
+                if debug:
+                    print(stack[len(stack)-1]," -> ready")
+                #-------------------------------------
+
+                ready.append(stack.pop(len(stack)-1))
+                no_not_term = 0
+                
+                return
+        
+rules = dict()
+
+rules['A'] = 'b','c'
+rules['B'] = 'cA','bb'
+string = ".B."
+
+stack = []
+ready = []
+stack.append(string)
+
+        
+while len(stack) > 0:
+    iterate_left(stack,ready,rules,False)
             
 print("\n Цепочки: \n")
 for i in range(len(ready)):
